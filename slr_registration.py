a1a55fb19992dfe0c142f022c7ba977677a9964d
Giulia Bertò <gberto@fbk.eu>
2018-01-16 18:05:35 +0100

    modified input argument in slr_registration.py

--- slr_registration.py
+++ slr_registration.py
@@ -11,76 +11,76 @@ import argparse
 import os.path
 import nibabel as nib
 import numpy as np
+import pickle
+from os.path import isfile
 from nibabel.streamlines import load
 from dipy.segment.clustering import QuickBundles
 from dipy.align.streamlinear import StreamlineLinearRegistration
 from dipy.tracking.streamline import set_number_of_points
 
 
-def tractograms_slr(target_subject_id, source_subject_id, src_dir):
-  
-	target_tractogram_filename = '%s/deterministic_tracking_dipy_FNAL/sub-%s/sub-%s_var-FNAL_tract.trk' %(src_dir, target_subject_id, target_subject_id)  
-	source_tractogram_filename = '%s/deterministic_tracking_dipy_FNAL/sub-%s/sub-%s_var-FNAL_tract.trk' %(src_dir, source_subject_id, source_subject_id)
-	print("Target tractogram filename: %s" %target_tractogram_filename)
-	print("Source tractogram filename: %s" %source_tractogram_filename)
+def tractograms_slr(moving_tractogram, static_tractogram):
 
 	print("Loading tractograms...")
-	target_tractogram = nib.streamlines.load(target_tractogram_filename)
-	target_tractogram = target_tractogram.streamlines
-	source_tractogram = nib.streamlines.load(source_tractogram_filename)
-	source_tractogram = source_tractogram.streamlines                  
+	moving_tractogram = nib.streamlines.load(moving_tractogram)
+	moving_tractogram = moving_tractogram.streamlines
+	static_tractogram = nib.streamlines.load(static_tractogram)
+	static_tractogram = static_tractogram.streamlines                  
 
-	print("Set parameters as in [Garyfallidis et al. 2015].") 
+	print("Set parameters as in Garyfallidis et al. 2015.") 
 	threshold_length = 40.0 # 50mm / 1.25
 	qb_threshold = 16.0  # 20mm / 1.25 
 	nb_res_points = 20
 
-	print("Performing QuickBundles of target tractogram and resampling...")
-	tt = np.array([s for s in target_tractogram if len(s) > threshold_length], dtype=np.object)
-	qb = QuickBundles(threshold=qb_threshold)
-	tt_clusters = [cluster.centroid for cluster in qb.cluster(tt)]
-	tt_clusters = set_number_of_points(tt_clusters, nb_res_points)
-
-	print("Performing QuickBundles of source tractogram and resampling...")
-	st = np.array([s for s in source_tractogram if len(s) > threshold_length], dtype=np.object)
+	print("Performing QuickBundles of static tractogram and resampling...")
+	st = np.array([s for s in static_tractogram if len(s) > threshold_length], dtype=np.object)
 	qb = QuickBundles(threshold=qb_threshold)
 	st_clusters = [cluster.centroid for cluster in qb.cluster(st)]
 	st_clusters = set_number_of_points(st_clusters, nb_res_points)
 
+	print("Performing QuickBundles of moving tractogram and resampling...")
+	mt = np.array([s for s in moving_tractogram if len(s) > threshold_length], dtype=np.object)
+	qb = QuickBundles(threshold=qb_threshold)
+	mt_clusters = [cluster.centroid for cluster in qb.cluster(mt)]
+	mt_clusters = set_number_of_points(mt_clusters, nb_res_points)
+
 	print("Performing Linear Registration...")
 	srr = StreamlineLinearRegistration()
-	srm = srr.optimize(static=tt_clusters, moving=st_clusters)
+	srm = srr.optimize(static=st_clusters, moving=mt_clusters)
 
 	print("Affine transformation matrix with Streamline Linear Registration:")
 	affine = srm.matrix
 	print('%s' %affine)
 
-	print("Applying the transformation...")
-	source_tractogram_aligned = srm.transform(source_tractogram)
-
-	return affine, srm, source_tractogram_aligned
+	return affine
 
 
 if __name__ == '__main__':
 
-	# Setting the location of the repository
-	script_src = os.path.basename(sys.argv[0]).strip('.py')
-	script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-	src_dir = os.path.abspath(os.path.join(script_dir, 'data/HCP3/derivatives'))
-	#out_dir = os.path.join(src_dir, 'source_tractogram_aligned')
-	#if not os.path.exists(out_dir):
-	#    os.mkdir(out_dir)   
-
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-target', nargs='?', const=1, default='',
-	                    help='The target subject id')
-	parser.add_argument('-source', nargs='?',  const=1, default='',
-	                    help='The source subject id')
-	                    
+	parser.add_argument('-moving', nargs='?', const=1, default='',
+	                    help='The moving tractogram filename')
+	parser.add_argument('-static', nargs='?',  const=1, default='',
+	                    help='The static tractogram filename')
+	parser.add_argument('-out', nargs='?',  const=1, default='default',
+	                    help='The output affine table filename .pickle')                    
 	args = parser.parse_args()
 
-	affine, srm, source_tractogram_aligned = tractograms_slr(args.target, args.source, src_dir)
+	table_filename = args.out
+	if isfile(table_filename):
+		print("Retrieving past results from %s" % table_filename)
+		table = pickle.load(open(table_filename))
+	else:
+		print("Creating a new table which will be saved in %s" % table_filename)
+		table = {}
 
-	sys.exit()
-
-	
+	key = tuple([(args.moving, args.static)])[0]
+	if table.has_key(key):
+		print("Affine already exist in %s" % table_filename)
+		affine = table[args.moving, args.static].items()[0][1]
+	else:	
+		affine = tractograms_slr(args.moving, args.static)	
+		table[args.moving, args.static] = {'affine': affine}
+		pickle.dump(table, open(table_filename, 'w'), protocol=pickle.HIGHEST_PROTOCOL)
+                                
+	sys.exit()    
