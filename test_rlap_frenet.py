8ed41c6bb7ff1160615e0527a698ddf77ccb3dde
Giulia Berto <gberto@hm015.karst.uits.iu.edu>
2018-04-29 18:25:03 -0400

    update

---
+++ test_rlap_frenet.py
@@ -0,0 +1,145 @@
+from __future__ import print_function
+import nibabel as nib
+import numpy as np
+from nibabel.streamlines import load, save
+from lap_single_example import compute_kdtree_and_dr_tractogram, RLAP, save_bundle
+from dipy.tracking.distances import bundles_distances_mam
+from compute_voxel_measures import compute_voxel_measures
+from sklearn.neighbors import KDTree
+import matplotlib.pyplot as plt
+from dissimilarity import compute_dissimilarity, dissimilarity
+from dipy.tracking.streamline import length, set_number_of_points
+from test_shape import frenet_diff
+import time
+
+
+try:
+    from linear_assignment import LinearAssignment
+except ImportError:
+    print("WARNING: Cythonized LAPJV not available. Falling back to Python.")
+    print("WARNING: See README.txt")
+    from linear_assignment_numpy import LinearAssignment
+
+
+def compute_lap_inputs(kdt, k, dm_source_tract, source_tract, tractogram, distance, nbp=12):
+    """Code for computing the inputs to the Rectangular Linear Assignment Problem.
+    """
+    tractogram = np.array(tractogram, dtype=np.object)
+    D, I = kdt.query(dm_source_tract, k=k)
+    superset = np.unique(I.flat)
+    np.save('superset_idx', superset)
+    print("Computing the distance matrix (%s x %s) for RLAP... " % (len(source_tract),
+                                                             len(superset)))
+    t0=time.time()
+    distance_matrix = dissimilarity(source_tract, tractogram[superset], distance)
+    print("Time for computing the distance matrix = %s seconds" %(time.time()-t0))
+    print("Computing the bundle similarity matrix (%s x %s) for RLAP... " % (len(source_tract),
+                                                             len(superset)))
+    t1=time.time()
+    frenet_matrix = frenet_diff(source_tract, tractogram[superset], nbp)
+    print("Time for computing the shape matrix = %s seconds" %(time.time()-t1))
+
+    return distance_matrix, frenet_matrix, superset
+
+
+def RLAP_modified(distance_matrix, frenet_matrix, superset, h):
+    """Code for MODIFIED Rectangular Linear Assignment Problem.
+    """
+    print("Computing cost matrix.")
+    cost_matrix = h * distance_matrix + (1-h) * frenet_matrix
+    print("Computing RLAP with LAPJV...")
+    assignment = LinearAssignment(cost_matrix).solution
+    estimated_bundle_idx = superset[assignment]
+    min_cost_values = cost_matrix[np.arange(len(cost_matrix)), assignment]
+
+    return estimated_bundle_idx, min_cost_values
+
+
+if __name__ == '__main__':
+
+	sub_list = ['100307', '109123', '131217', '199655', '341834']#, '599671', '601127', '756055', '770352', '917255']
+	distance_list = ['mam']#['mam', 'varifolds']
+	bundle_list = ['ufL']#, 'cstL']#, 'ufR', 'cstL', 'cstR', 'ifofL', 'ifofR', 'thprefL', 'thprefR']
+	registration = ['ant4fa']#['slr']#'ant4t1w', 'ant4fa']
+	method = ['rlap_frenet'] #rlap
+	h_list = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
+	k = 500
+
+	np.random.seed(0)
+
+	basedir = 'miccai2018_dataset'
+	#DSC_nn = np.zeros((len(sub_list), len(bundle_list), len(sub_list), len(registration), len(h_list)))
+	#DSC_rlap = np.zeros((len(sub_list), len(bundle_list), len(sub_list), len(registration), len(h_list)))
+	DSC_rlap_frenet = np.zeros((len(sub_list), len(bundle_list), len(sub_list), len(registration), len(h_list)))
+
+	for ss, static_sub in enumerate(sub_list):
+	    if ss == 1:
+		tractogram_dir = 'deterministic_tracking_dipy_FNAL'
+		static_tractogram_filename = '%s/%s/sub-%s/sub-%s_var-FNAL_tract.trk' %(basedir, tractogram_dir, static_sub, static_sub)
+		static_tractogram = nib.streamlines.load(static_tractogram_filename)
+		static_tractogram = static_tractogram.streamlines
+		
+		for d, dist in enumerate(distance_list):
+			if dist == 'mam':
+				distance_func = bundles_distances_mam
+			elif dist == 'varifolds':
+				distance_func = varifolds
+			else:
+				print("Distance %s not supported yet." % dist)
+        		Exception		 	
+			print("Compute the dissimilarity representation of the static tractogram and build the kd-tree.")
+			kdt, prototypes = compute_kdtree_and_dr_tractogram(static_tractogram, distance=distance_func)
+
+			for b, bundle in enumerate(bundle_list):
+				wmql_dir = 'wmql_FNALW' 
+				true_bundle_filename = '%s/%s/sub-%s/sub-%s_var-FNALW_set-%s_tract.trk' %(basedir, wmql_dir, static_sub, static_sub, bundle)
+				true_bundle = nib.streamlines.load(true_bundle_filename)
+				true_bundle = true_bundle.streamlines
+
+				for ms, moving_sub in enumerate(sub_list):
+					if moving_sub != static_sub:
+						for r, reg in enumerate(registration):
+							if reg == 'slr':
+								slr_dir = basedir + '/streamline_based_affine_registration'
+								bundle_filename = '%s/sub-%s/sub-%s_var-slr_space-%s_set-%s_tract.trk' %(slr_dir, moving_sub, moving_sub, static_sub, bundle)
+							else:
+								ants_dir = basedir + '/voxel_based_registration'
+								bundle_filename = '%s/sub-%s/sub-%s_space_%s_var-%s_set-%s_tract.trk' %(ants_dir, moving_sub, moving_sub, static_sub, reg, bundle)
+							example_bundle = nib.streamlines.load(bundle_filename)
+							example_bundle = example_bundle.streamlines
+							print("Compute the dissimilarity of the aligned example bundle with the prototypes of static tractogram.")
+							moving_example = np.array(example_bundle, dtype=np.object)
+							dm_moving_example = distance_func(moving_example, prototypes)
+
+							for m, met in enumerate(method):
+								if met == 'rlap':
+									print("Segmentation as Rectangular linear Assignment Problem (RLAP).")
+									estimated_bundle_idx, min_cost_values = RLAP(kdt, k, dm_moving_example, moving_example, static_tractogram, distance_func)
+									estimated_bundle = static_tractogram[estimated_bundle_idx]
+									print("Computing the DSC value.")
+									DSC, TP, vol_A, vol_B = compute_voxel_measures(estimated_bundle, true_bundle)	
+									print("The DSC value is %s" %DSC)
+									DSC_rlap[ss, b, ms, r, d] = DSC
+								if met == 'rlap_frenet':
+									print("Segmentation as MODIFIED WITH FRENET Rectangular linear Assignment Problem (RLAP).")
+									distance_matrix, frenet_matrix, superset = compute_lap_inputs(kdt, k, dm_moving_example, moving_example, static_tractogram, distance_func)
+									d_matrix_name = 'D_m%s_s%s' %(moving_sub, static_sub)
+									f_matrix_name = 'F_m%s_s%s' %(moving_sub, static_sub)
+									np.save(d_matrix_name, distance_matrix)
+									np.save(f_matrix_name, frenet_matrix)
+
+									#normalize matrices
+									frenet_matrix1=(frenet_matrix-np.min(frenet_matrix))/(np.max(frenet_matrix)-np.min(frenet_matrix))
+									distance_matrix1=(distance_matrix-np.min(distance_matrix))/(np.max(distance_matrix)-np.min(distance_matrix))
+									for hh, h in enumerate(h_list):
+										estimated_bundle_idx, min_cost_values = RLAP_modified(distance_matrix1, frenet_matrix1, superset, h)
+										estimated_bundle = static_tractogram[estimated_bundle_idx]
+										print("Computing the DSC value.")
+										DSC, TP, vol_A, vol_B = compute_voxel_measures(estimated_bundle, true_bundle)	
+										print("The DSC value with h=%s is %s" %(h,DSC))
+										DSC_rlap_frenet[ss, b, ms, r, hh] = DSC
+
+
+	#np.save('DSC_nn_90', DSC_nn)
+	#np.save('DSC_rlap_90', DSC_rlap)
+	np.save('DSC_rlap_frenet_3', DSC_rlap_frenet)
---
+++ test_shape.py
@@ -0,0 +1,178 @@
+from __future__ import division
+import nibabel as nib
+from nibabel.streamlines import load
+from dipy.tracking.metrics import winding
+import numpy as np
+from dipy.tracking.streamline import length, set_number_of_points
+import math
+from dipy.tracking.distances import bundles_distances_mam
+import time
+from dipy.tracking import metrics as tm
+
+
+def compute_angle_vector(str1,str2):
+    angle_vector=np.zeros(len(str1)-1)
+    for i in range(len(angle_vector)):
+        seg_a=str1[i:i+2]
+        seg_b=str2[i:i+2]
+        angle_vector[i]=compute_angle(seg_a,seg_b)
+    return angle_vector    
+
+def compute_angle(seg_a,seg_b):
+    offset=seg_a[0]-seg_b[0]
+    c=np.concatenate((seg_a,seg_b+offset), axis=0)
+    c=c[1:len(c)]
+    angle=winding(c)
+    if (abs(angle-360)<10e-4 or math.isnan(angle)):
+        angle = 0.0
+    elif (angle>180):
+        angle = angle - 180          
+    rad=np.deg2rad(angle)
+    return rad
+
+def compute_shape_similarity_new(str1,str2):
+    step_size1 = length(str1)/len(str1)
+    step_size2 = length(str2)/len(str2)
+    alpha = compute_angle_vector(str1,str2)
+    sim_num = (step_size1+step_size2)*np.sum(alpha)
+    sim_den=math.pi*(length(str1)+length(str2))    
+    sim=1-(sim_num/sim_den)  
+    return sim
+
+def compute_shape_similarity(str1,str2):
+    sim_num=0
+    for i in range(len(str1)-1):
+        seg_a=str1[i:i+2]
+        seg_b=str2[i:i+2]
+        num=(length(seg_a)+length(seg_b))*compute_angle(seg_a,seg_b)
+        sim_num=sim_num+num
+    sim_den=math.pi*(length(str1)+length(str2))    
+    sim=1-(sim_num/sim_den)  
+    return sim    
+
+def compute_bundle_shape_similarity_new(streamlines_A, streamlines_B):
+    result = np.zeros((len(streamlines_A), len(streamlines_B)))
+    for i, sa in enumerate(streamlines_A):
+        for j, sb in enumerate(streamlines_B):
+            result[i, j] = compute_shape_similarity_new(sa, sb)
+    return result
+
+def compute_bundle_shape_similarity(streamlines_A, streamlines_B):
+    result = np.zeros((len(streamlines_A), len(streamlines_B)))
+    for i, sa in enumerate(streamlines_A):
+        for j, sb in enumerate(streamlines_B):
+            result[i, j] = compute_shape_similarity(sa, sb)
+    return result    
+
+def compute_bundle_shape_similarity_flip(streamlines_A, streamlines_B):
+    result1 = np.zeros((len(streamlines_A), len(streamlines_B)))
+    result2 = np.zeros((len(streamlines_A), len(streamlines_B)))
+    for i, sa in enumerate(streamlines_A):
+        for j, sb in enumerate(streamlines_B):
+            result1[i, j] = compute_shape_similarity_new(sa, sb)
+	    result2[i, j] = compute_shape_similarity_new(sa, np.flip(sb, axis=0))
+	print("Row %s done." %i)
+    return np.fmax(result1, result2) 
+
+def winding_diff(streamlines_A, streamlines_B):
+    result = np.zeros((len(streamlines_A), len(streamlines_B)))
+    for i, sa in enumerate(streamlines_A):
+	for j, sb in enumerate(streamlines_B):
+	    result[i, j] = abs(winding(sa)-winding(sb))
+	print("Row %s done." %i)
+    return result
+
+def curvature_diff(streamlines_A, streamlines_B):
+    result = np.zeros((len(streamlines_A), len(streamlines_B)))
+    for i, sa in enumerate(streamlines_A):
+        for j, sb in enumerate(streamlines_B):
+            result[i, j] = abs(mean_curvature(sa)-mean_curvature(sb))
+	print("Row %s done." %i)
+    return result
+
+def frenet_diff(streamlines_A, streamlines_B, nbp=12):
+    streamlines_A_res = np.array([set_number_of_points(s, nb_points=nbp)
+                               for s in streamlines_A])
+    streamlines_B_res = np.array([set_number_of_points(s, nb_points=nbp)
+                               for s in streamlines_B])
+    result = np.zeros((len(streamlines_A_res), len(streamlines_B_res)))
+    for i, sa in enumerate(streamlines_A_res):
+	Ta,Na,Ba,ka,ta=tm.frenet_serret(sa)
+        for j, sb in enumerate(streamlines_B_res):
+	    Tb,Nb,Bb,kb,tb=tm.frenet_serret(sb)
+	    m = np.sum(np.square(ka-kb))
+	    n = np.sum(np.square(ka-np.flip(kb,axis=0)))
+            result[i, j] = min(m,n)
+	#print("Row %s done." %i)
+    return result
+
+
+if __name__ == '__main__':
+
+    # Load data
+    #ioff_left_trk = nib.streamlines.load('/home/giulia/Desktop/test_shape/sub-660951_var-FNALW_ioff.left.trk')
+    #ioff_right_trk = nib.streamlines.load('/home/giulia/Desktop/test_shape/sub-660951_var-FNALW_ioff.right.trk')
+    #ioff_left_trk = nib.streamlines.load('/home/giulia/prni2017_streamline_distances/code/data/100307/wmql_tracts/100307_ifof.left.trk')
+    #ioff_right_trk = nib.streamlines.load('/home/giulia/prni2017_streamline_distances/code/data/100307/wmql_tracts/100307_ifof.right.trk')
+    cbL = nib.streamlines.load('/N/u/gberto/Karst/LAP_tract_segmentation/0007_Left_Cingulum_Cingulate_tract_IU5.trk')
+    cbL_true = nib.streamlines.load('/N/u/gberto/Karst/LAP_tract_segmentation/Left_Cingulum_Cingulate_tract.trk')
+
+
+    # Loading with trackvis.read
+    #ioff_left_trk, hdr = nib.trackvis.read('/home/giulia/Desktop/test_shape/sub-660951_var-FNALW_ioff.left.trk')
+    #ioff_left_trk = np.array([streamline[0] for streamline in ioff_left_trk], dtype=np.object)
+    #ioff_right_trk, hdr = nib.trackvis.read('/home/giulia/Desktop/test_shape/sub-660951_var-FNALW_ioff.right.trk')
+    #ioff_right_trk = np.array([streamline[0] for streamline in ioff_right_trk], dtype=np.object)
+
+    np.set_printoptions(precision=3)
+
+    # Extract 2 streamlines
+    #str1 = ioff_left_trk.streamlines[0] 
+    #str1 = ioff_left_trk[0]
+    str1 = cbL.streamlines[0]
+    print("Length first streamline: %s" %len(str1)) 
+    #str2 = ioff_left_trk.streamlines[1] 
+    #str2 = ioff_left_trk[1]
+    str2 = cbL.streamlines[0]
+    print("Length second streamline: %s" %len(str2)) 
+
+    # Resample streamlines
+    nbp=12
+    str1_res = set_number_of_points(str1, nb_points=nbp)
+    str2_res = set_number_of_points(str2, nb_points=nbp)
+    print("Length first streamline after resample: %s" %len(str1_res)) 
+    print("Length second streamline after resample: %s" %len(str2_res)) 
+
+    # Compute similarity measure
+    similarity_coeff = compute_shape_similarity(str1_res, str2_res)
+    print("Similarity coefficient: %s" %similarity_coeff)
+
+    # Compute bundle similarity matrix 
+    #streamlines_A = ioff_left_trk.streamlines[0:100]
+    #streamlines_B = ioff_right_trk.streamlines[0:150]
+    #streamlines_A = ioff_left_trk[0:100]
+    #streamlines_B = ioff_right_trk[0:150]
+    streamlines_A = cbL.streamlines[0:200]
+    streamlines_B = cbL_true.streamlines[0:300]
+    streamlines_A_res = np.array([set_number_of_points(s, nb_points=nbp)
+                               for s in streamlines_A])
+    streamlines_B_res = np.array([set_number_of_points(s, nb_points=nbp)
+                               for s in streamlines_B])
+    t0=time.time()
+    similarity_bundle_matrix = compute_bundle_shape_similarity(streamlines_A_res, streamlines_B_res)
+    t1=time.time()
+    #print("Similarity bundle matrix: %s" %similarity_bundle_matrix)
+
+    #Compute distance matrix
+    t2=time.time()
+    D = bundles_distances_mam(streamlines_A, streamlines_B)
+    t3=time.time()
+    #print("Distance matrix: %s" %D)
+
+    t4=time.time()
+    similarity_bundle_matrix_new = compute_bundle_shape_similarity_new(streamlines_A_res, streamlines_B_res)
+    t5=time.time()
+
+    print("Time to compute the shape matrix = %s seconds" %(t1-t0))
+    print("Time to compute the distance matrix = %s seconds" %(t3-t2))
+    print("Time to compute the shape matrix new = %s seconds" %(t5-t4))
